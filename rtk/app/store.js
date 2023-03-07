9e8505961d86343349ced6a0fb852a18c349564a
yakubsiyam <itsyakubsiyam@gmail.com>
2023-03-07 16:18:48 +0600

    Adding middlewares

--- rtk/app/store.js
+++ rtk/app/store.js
@@ -1,11 +1,17 @@
 const { configureStore } = require("@reduxjs/toolkit");
 const counterReducer = require("../features/counter/counterSlice");
+const dynamicCounterReducer = require("../features/dynamicCounter/dynamicCounterSlice");
+const {createLogger} = require("redux-logger");
+
+const logger = createLogger();
 
 // configure store
 const store = configureStore({
     reducer: {
         counter: counterReducer,
-    }
-})
+        dynamicCounter: dynamicCounterReducer
+    },
+    middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(logger)
+});
 
 module.exports = store;
--- rtk/index.js
+++ rtk/index.js
@@ -1,12 +1,13 @@
 const store = require("./app/store");
 const {counterActions} = require("./features/counter/counterSlice");
+const {dynamicCounterActions} = require("./features/dynamicCounter/dynamicCounterSlice");
 
 //Initial state
-console.log(`Initial State: ${JSON.stringify(store.getState())}`);
+// console.log(`Initial State: ${JSON.stringify(store.getState())}`);
 
 // subscribe to state changes
 store.subscribe(() => {
-    console.log(store.getState());
+    // console.log(store.getState());
 });
 
 // disptach actions
@@ -15,3 +16,9 @@ store.dispatch(counterActions.increment());
 store.dispatch(counterActions.increment());
 
 store.dispatch(counterActions.decrement());
+
+store.dispatch(dynamicCounterActions.increment(3));
+
+store.dispatch(dynamicCounterActions.increment(4));
+
+store.dispatch(dynamicCounterActions.decrement(2));
