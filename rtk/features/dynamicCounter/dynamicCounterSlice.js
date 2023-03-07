2fec310f2cfacb9da9e22a319437dfc364e3e0bb
yakubsiyam <itsyakubsiyam@gmail.com>
2023-03-07 16:59:47 +0600

    Add Extra Reducer concept

--- rtk/features/dynamicCounter/dynamicCounterSlice.js
+++ rtk/features/dynamicCounter/dynamicCounterSlice.js
@@ -1,4 +1,5 @@
 const { createSlice } = require("@reduxjs/toolkit");
+const {counterActions} = require("../counter/counterSlice");
 
 // initial state
 const initialState = {
@@ -15,6 +16,17 @@ const dynamicCounterSlice = createSlice({
         decrement: (state, action) => {
             state.count -= action.payload;
         }
+    },
+    // extraReducers: {
+    //     ["counter/increment"]: (state, action) => {
+    //         state.count++;
+    //     }
+    // },
+
+    extraReducers: (builder) => {
+        builder.addCase(counterActions.increment, (state, action) => {
+            state.count += 1;
+        })
     }
 });
 
